ebd3619bb25add607ebe32a27c37205a11a282d5
riaz <riaz.2012@gmail.com>
2024-06-24 00:10:01 -0700

    part2

--- research/SqliteClone/main.c
+++ research/SqliteClone/main.c
@@ -6,9 +6,16 @@
 
 typedef enum {
     META_COMMAND_SUCCESS,
-    META_COMMAND_RECOGNIZED_COMMAND
+    META_COMMAND_UNRECOGNIZED_COMMAND
 } MetaCommandResult;
 
+typedef enum { PREPARE_SUCCESS, PREPARE_UNRECOGNIZED_STATEMENT} PrepareResult;
+
+typedef enum { STATEMENT_INSERT, STATEMENT_SELECT} StatementType;
+
+typedef struct {
+    StatementType type;
+} Statement;
 
 // Defining a InputBuffer for the REPL
 typedef struct {
@@ -27,6 +34,49 @@ InputBuffer* new_input_buffer() {
     return input_buffer;
 }
 
+// closing the input buffer
+void close_input_buffer(InputBuffer *buf) {
+    free(buf->buffer);
+    free(buf);
+}
+
+// This method manages exiting from the REPL using the MetaCommandResult enum
+MetaCommandResult do_meta_command(InputBuffer* buf) {
+    if (strcmp(buf->buffer, ".exit") == 0) {
+        close_input_buffer(buf);
+        exit(EXIT_SUCCESS);
+    }else{
+        return META_COMMAND_UNRECOGNIZED_COMMAND;
+    }
+}
+
+// This method populates the Statement struct based on the REPL stage
+PrepareResult prepare_statement(InputBuffer* buf, Statement* stmt) {
+    if (strncmp(buf->buffer, "insert", 6) == 0) {
+        stmt->type = STATEMENT_INSERT;
+        return PREPARE_SUCCESS;
+    }else if (strncmp(buf->buffer, "select", 6) == 0) {
+        stmt->type = STATEMENT_SELECT;
+        return PREPARE_SUCCESS;
+    }
+    return PREPARE_UNRECOGNIZED_STATEMENT;
+}
+
+// a void executer
+void execute_statement(Statement* stmt) {
+    switch(stmt->type) {
+        case STATEMENT_INSERT:
+            printf("This is where we would do an insert\n");
+            break;
+        case STATEMENT_SELECT:
+            printf("This is where we would do a select\n");
+            break;
+        default:
+            printf("Unsupported staetement encountered");
+    }
+}
+
+
 // Reading input
 void read_input(InputBuffer *buf) {
     ssize_t bytes_read = getline(&(buf->buffer), &(buf->buffer_length), stdin);
@@ -42,11 +92,6 @@ void read_input(InputBuffer *buf) {
     buf->buffer[bytes_read - 1] = 0;
 }
 
-// closing the input buffer
-void close_input_buffer(InputBuffer *buf) {
-    free(buf->buffer);
-    free(buf);
-}
 
 // default prompt for the repl
 void print_prompt() {
@@ -66,13 +111,32 @@ int main(int argc, char* argv[]) {
         print_prompt(); // this is the default prompt
         read_input(buf); 
 
-        // checking for special conditions
-        if (strcmp(buf->buffer, ".exit") == 0) {
-            // the user intended to exit 
-            close_input_buffer(buf);
-            exit(EXIT_SUCCESS);
-        } else{
-            printf("Unrecognized Command '%s'. \n", buf->buffer);
+        // we will check if a command was passed and try to exeucte it
+
+        // we check if the statement begins with ., and next we validate if its valid
+        if (buf->buffer[0] == '.') {
+            switch (do_meta_command(buf)) {
+                case META_COMMAND_SUCCESS:
+                    continue; // all good
+                case META_COMMAND_UNRECOGNIZED_COMMAND:
+                    printf("Unrecognized Command %s \n", buf->buffer);
+                    continue;
+            }
         }
+
+        // next we need to initialize statement to register and execute the query
+        Statement statement;
+        switch (prepare_statement(buf, &statement)) {
+            case PREPARE_SUCCESS:
+                break;
+            case PREPARE_UNRECOGNIZED_STATEMENT:
+                printf("Unrecognized Keyword at start %s \n", buf->buffer);
+                continue; // we want the repl to continue but not proceed for this iteration
+        }
+
+        // now we just execute the statement
+        execute_statement(&statement);
+
+        printf("Done Executing\n");
     }
 }
